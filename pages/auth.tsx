40456dc9457123df7c32342fa76035edfc481616
technoprogramming92 <technoprogramming92@gmail.com>
2023-03-21 08:35:02 +0530

    Google and github auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -90,6 +90,7 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
+              onClick={() => signIn('google', { callbackUrl: '/' })}
                 className="
                             w-10 h-10
                             bg-white
@@ -104,6 +105,7 @@ const Auth = () => {
                 <FcGoogle size={30} />
               </div>
               <div
+              onClick={() => signIn('github', { callbackUrl: '/' })}
                 className="
                             w-10 h-10
                             bg-white
